ff113e9c7a2ca19e637070b395452845e956b1f1
Abdulrahman-19 <abedabogneem@gmail.com>
2021-03-04 16:10:25 +0200

    new-js

---
+++ java.js
@@ -0,0 +1,17 @@
+var a = 11 ;
+var b = 6 ;
+var c = a + b ;
+console.log( c ) ;
+alert("hi");
+var city = prompt("enter your city")
+var g = '<h1>'+ 'wellcom to irbid' +'<h1>';
+confirm("are you from jordan");
+
+if (city==("irbid")) {
+    alert("you are from irbid");
+
+} else { 
+    alert("you are not from irbid");
+};
+
+document.write(g);
